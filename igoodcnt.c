5460a18d8a4f48307d5631d086b9657c9b5dcf5b
mvanbraeckel <mvanbrae@uoguelph.ca>
2019-03-31 13:48:07 -0600

    automatic backup via makefile

--- igoodcnt.c
+++ igoodcnt.c
@@ -1,27 +1,34 @@
+/**
+ * Mitchell Van Braeckel (mvanbrae@uoguelph.ca) 1002297
+ * 31/03/2019
+ * CIS*3110: Operating Systems A4 - Synchronizing Threads with POSIX Semaphores
+ * --> Uses semaphores to synchronously increment a global variable being
+ * 		accessed by the same two threads
+ */
+
+// ============================== INCLUDES ==============================
 #include <pthread.h>
 #include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 
+// ========================== GLOBAL VARIABLES ==========================
 int NITER = 1000000;
-
 int cnt = 0;
+sem_t mutex;
 
-void * Count(void * a)
-{
-    int i, tmp;
-    for(i = 0; i < NITER; i++)
-    {
-        tmp = cnt;      /* copy the global cnt locally */
-        tmp = tmp+1;    /* increment the local copy */
-        cnt = tmp;      /* store the local value into the global cnt */ 
-    }
-	return NULL;
-}
+// ======================== FUNCTION PROTOTYPES =========================
+void* Count();
+void* thread();
 
-int main(int argc, char * argv[])
-{
+/**
+ * Accepts a single cmd line integer argument for NITER value
+ */
+int main(int argc, char * argv[]) {
+	// declare variables
 	pthread_t tid1, tid2;
+
+	sem_init(&mutex, 0, 1); // initialize the semaphor
 
 	// validate arguments
 	// implementation
@@ -39,42 +46,62 @@ int main(int argc, char * argv[])
 	printf("2*NITER is [%d]\n", 2*NITER);
 // End of code section 
 
-	// creating Thread 1
-	if(pthread_create(&tid1, NULL, Count, NULL))
-	{
+	// create both threads
+	if(pthread_create(&tid1, NULL, thread, NULL)) {
 		printf("\n ERROR creating thread 1");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
-
-	// creating Thread 2
-	if(pthread_create(&tid2, NULL, Count, NULL))
-	{
+	if(pthread_create(&tid2, NULL, thread, NULL)) {
 		printf("\n ERROR creating thread 2");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
-	if(pthread_join(tid1, NULL))	/* wait for the thread 1 to finish */
-	{
+	// wait for each thread to finish
+	if(pthread_join(tid1, NULL)) {
 		printf("\n ERROR joining thread");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
-
-	if(pthread_join(tid2, NULL))        /* wait for the thread 2 to finish */
-	{
+	if(pthread_join(tid2, NULL)) {
 		printf("\n ERROR joining thread");
-		exit(1);
+		exit(EXIT_FAILURE);
 	}
 
-        // Display the value of count cnt
+    // Display the value of count cnt
 // PLEASE DO NOT remove or modify the following code
 	printf("\nCounter is [%d]\n", cnt);
-	if (cnt == 2 * NITER) 
+	if(cnt == 2 * NITER) {
 		printf("\nPASS\n");
-	else
+	} else {
 		printf("\nFAIL\n");
+	}
 // End of code section
 
+	sem_destroy(&mutex); // destroy the semaphor
 	pthread_exit(NULL);
+	return 0;
+} // end main
+
+// ======================================================================
+
+/**
+ * Increments the global count NITER times
+ */
+void* Count() {
+    int i, tmp;
+    for(i = 0; i < NITER; i++) {
+        tmp = cnt;      // copy the global cnt locally
+        tmp = tmp+1;    // increment the local copy
+        cnt = tmp;      // store the local value into the global cnt 
+    }
+	return NULL;
 }
 
-
+/**
+ * Uses semaphores to increment the global count NITER times while blocking other thread
+ */
+void* thread() {
+	sem_wait(&mutex);	// wait on the semaphore
+	Count();			// inrement count in the critical section
+	sem_post(&mutex);	// increment value of the semaphore (signal)
+	return NULL;
+}
